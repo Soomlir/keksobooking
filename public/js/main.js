ae3909c24434b3785c0a61a53cdade0f3de2024a
Soomlir <Soomlir@yandex.ru>
2023-02-12 23:42:48 +0300

    4rd task edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,21 @@
+const MIN_LAT = 35.65;
+const MAX_LAT = 35.7;
+const MIN_LNG = 139.7;
+const MAX_LNG = 139.8;
+const COORD_PRECISION = 5;
+const MAX_PRICE = 100000;
+const MAX_ROOMS = 10;
+
+const HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME_CHECKS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
 const getRandomIntNumber = (min, max) => {
   if (min === max) {
     return max;
@@ -22,59 +40,51 @@ const getRandomFloatNumber = (min, max, count) => {
   return (Math.random() * (max - min) + min).toFixed(count);
 };
 
+const getRandomArrayItem = (arr) => arr[getRandomIntNumber(0, arr.length)];
+
+const getRandomArrayFragment = (arr) => {
+  const arrStart = getRandomIntNumber(0, arr.length);
+  const arrEnd = getRandomIntNumber(0, arr.length);
+
+  return arr.slice(arrStart, arrEnd);
+};
+
 const createObj = (i) => {
   const index = i + 1;
   const numberWithZero = `${index < 10 ? 0 : ''}${index}`;
 
-  const MIN_LAT = 35.65;
-  const MAX_LAT = 35.7;
-  const MIN_LNG = 139.7;
-  const MAX_LNG = 139.8;
-  const COORD_PRECISION = 5;
+  const location = {
+    lat: getRandomFloatNumber(MIN_LAT, MAX_LAT, COORD_PRECISION),
+    lng: getRandomFloatNumber(MIN_LNG, MAX_LNG, COORD_PRECISION)
+  };
 
-  const maxPrice = 20000;
-  const PRICE = getRandomIntNumber(1, maxPrice);
-  const maxRooms = 7;
-  const ROOMS = getRandomIntNumber(1, maxRooms);
-  const GUESTS = getRandomIntNumber(1, 10);
+  const price = getRandomIntNumber(1, MAX_PRICE);
+  const rooms = getRandomIntNumber(1, MAX_ROOMS);
+  const guests = getRandomIntNumber(1, 10);
 
-  const typeOfHousing = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const TYPE = typeOfHousing[getRandomIntNumber(0, typeOfHousing.length)];
-
-  const timeCheck = ['12:00', '13:00', '14:00'];
-  const CHECKIN = timeCheck[getRandomIntNumber(0, timeCheck.length)];
-  const CHECKOUT = timeCheck[getRandomIntNumber(0, timeCheck.length)];
-
-  const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-  const obj =  {
+  return  {
     author: {
       avatar: `img/avatars/user${numberWithZero}.png`,
     },
 
     offer: {
-      title: 1,
+      title: `Объявление № ${index}`,
       address: `${location.lat} ${location.lng}`,
-      price: PRICE,
-      type: TYPE,
-      rooms: ROOMS,
-      guests: GUESTS,
-      checkin: CHECKIN,
-      checkout: CHECKOUT,
-      features: features,
-      description: 1,
-      photos: 1,
+      price,
+      type: getRandomArrayItem(HOUSING_TYPES),
+      rooms,
+      guests,
+      checkin: getRandomArrayItem(TIME_CHECKS),
+      checkout: getRandomArrayItem(TIME_CHECKS),
+      features: getRandomArrayFragment(FEATURES),
+      description: `Помещение с ценой ${price}, количеством комнат ${rooms}, на ${guests} гостей`,
+      photos: getRandomArrayFragment(PHOTOS),
     },
 
-    location: {
-      lat: getRandomFloatNumber(MIN_LAT, MAX_LAT, COORD_PRECISION),
-      lng: getRandomFloatNumber(MIN_LNG, MAX_LNG, COORD_PRECISION)
-    }
+    location
   };
-
-  return obj;
 };
 
 const getRandomOffers = (length) => Array.from({ length }, (_el, i) => createObj(i + 1));
 
-getRandomOffers();
+getRandomOffers(10);
