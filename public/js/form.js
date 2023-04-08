a9b55d4f970472e8d251535687b849599f299d39
Soomlir <Soomlir@yandex.ru>
2023-04-08 21:17:56 +0300

    validate

--- public/js/form.js
+++ public/js/form.js
@@ -1,74 +1,40 @@
-import { initValidator } from './validator.js';
-import {
-  ROOM_NUMBERS_GUESTS,
-  MIN_TITLE_LENGTH,
-  MAX_TITLE_LENGTH,
-  MAX_VALUE_PRICE,
-  PRIORITY,
-  PRICE_DICTIONARY
-} from './constants.js';
+// import { initValidator } from './validator.js';
+// import { ROOM_NUMBERS_GUESTS, PRICE_DICTIONARY } from './constants.js';
 
-const forms = ['ad-form', 'map__filters'].map((selector) => ({
-  element: document.querySelector(`.${selector}`),
-  disabledClass: `${selector}--disabled`
-}));
-const formElements = document.querySelectorAll('fieldset, select');
+// const forms = ['ad-form', 'map__filters'].map((selector) => ({
+//   element: document.querySelector(`.${selector}`),
+//   disabledClass: `${selector}--disabled`
+// }));
+// const formElements = document.querySelectorAll('fieldset, select');
 
-const toggleForms = (activate) => {
-  const method = activate ? 'remove' : 'add';
-  forms.forEach(({ element, disabledClass }) => {
-    element.classList[method](disabledClass);
-  });
+// const toggleForms = (activate) => {
+//   const method = activate ? 'remove' : 'add';
+//   forms.forEach(({ element, disabledClass }) => {
+//     element.classList[method](disabledClass);
+//   });
 
-  formElements.forEach((element) => element.disabled === !activate);
-};
+//   formElements.forEach((element) => element.disabled === !activate);
+// };
 
-const validator = initValidator(forms);
+// const validateCapacity = () => ROOM_NUMBERS_GUESTS[roomNumberElement.value].includes(capacityElement.value);
 
-const titleElement = forms.querySelector('#title');
-const priceElement = forms.querySelector('#price');
-const roomNumberElement = forms.querySelector('#room_number');
-const capacityElement = forms.querySelector('#capacity');
-const typeHousingElement = forms.querySelector('#type');
 
-validator.addValidator(
-  titleElement,
-  ({ length }) => length >= MIN_TITLE_LENGTH && length <= MAX_TITLE_LENGTH,
-  `Не менее ${MIN_TITLE_LENGTH} символов и не более ${MAX_TITLE_LENGTH} символов`,
-  PRIORITY,
-  false
-);
+// const validateTypeHousing = (evt) => {
+//   priceElement.placeholder = evt.target.value;
+//   priceElement.value = evt.target.value;
 
-validator.addValidator(
-  priceElement,
-  (value) => value <= MAX_VALUE_PRICE,
-  `Максимальное значение ${MAX_VALUE_PRICE}`,
-  PRIORITY,
-  false
-);
+//   if (priceElement.value >= PRICE_DICTIONARY[evt.target.value]) {
+//     return true;
+//   }
 
-const validateCapacity = () => {
-  validator.addValidator(
-    capacityElement,
-    () => ROOM_NUMBERS_GUESTS[roomNumberElement.value].includes(capacityElement.value),
-    '',
-    PRIORITY,
-    false
-  );
-};
+//   return false;
+// };
 
-const validateTypeHousing = () => {
-  validator.addValidator(
-    priceElement,
-    () => priceElement.value >= PRICE_DICTIONARY[typeHousingElement],
-    '',
-    PRIORITY,
-    false
-  );
-};
+// roomNumberElement.addEventListener('change', validateCapacity);
+// typeHousingElement.addEventListener('change', validateTypeHousing);
 
-roomNumberElement.addEventListener('change', validateCapacity);
-typeHousingElement.addEventListener('input', validateTypeHousing);
+// const validator = initValidator(forms);
 
-export const activateForms = () => toggleForms(true);
-export const deactivateForms = () => toggleForms(false);
+// export const activateForms = () => toggleForms(true);
+// export const deactivateForms = () => toggleForms(false);
+// export { validateCapacity, validateTypeHousing };
--- public/js/validator.js
+++ public/js/validator.js
@@ -1,11 +1,38 @@
-// import { form } from './form.js';
+// import { validateCapacity, validateTypeHousing } from './form.js';
+// import { MIN_TITLE_LENGTH, MAX_TITLE_LENGTH, MAX_VALUE_PRICE, PRIORITY } from './constants.js';
 
-const initValidator = ({ formElement }) => {
-  const validator = new Pristine(formElement, {
-    // TODO нужные опции
-  });
+// const initValidator = ({dataElements}) => {
+//   const validator = new Pristine(form, {
+//     // TODO нужные опции
+//   });
 
-  return validator;
-};
+//   const titleElement = document.querySelector('#title');
+//   const priceElement = document.querySelector('#price');
+//   const roomNumberElement = document.querySelector('#room_number');
+//   const capacityElement = document.querySelector('#capacity');
+//   const typeHousingElement = document.querySelector('#type');
 
-export { initValidator };
+//   validator.addValidator(
+//     titleElement,
+//     ({ length }) => length >= MIN_TITLE_LENGTH && length <= MAX_TITLE_LENGTH,
+//     `Не менее ${MIN_TITLE_LENGTH} символов и не более ${MAX_TITLE_LENGTH} символов`,
+//     PRIORITY,
+//     false
+//   );
+
+//   validator.addValidator(
+//     priceElement,
+//     (value) => value <= MAX_VALUE_PRICE,
+//     `Максимальное значение ${MAX_VALUE_PRICE}`,
+//     PRIORITY,
+//     false
+//   );
+
+//   validator.addValidator(capacityElement, validateCapacity, '', PRIORITY, false);
+
+//   validator.addValidator(priceElement, validateTypeHousing, '', PRIORITY, false);
+
+//   return validator;
+// };
+
+// export { initValidator };
