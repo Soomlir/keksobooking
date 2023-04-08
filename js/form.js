bd307a8c4dcdaa51ecbcdcfaaf4c63d65e9a6850
Soomlir <Soomlir@users.noreply.github.com>
2023-04-08 19:00:45 +0000

    deploy: 64a26c3e2c9063c82b49574aa0b80ecd546daad3

--- js/form.js
+++ js/form.js
@@ -1,40 +1,54 @@
-// import { initValidator } from './validator.js';
-// import { ROOM_NUMBERS_GUESTS, PRICE_DICTIONARY } from './constants.js';
+import { initValidator } from './validator.js';
+import { ROOM_NUMBERS_GUESTS, PRICE_DICTIONARY } from './constants.js';
 
-// const forms = ['ad-form', 'map__filters'].map((selector) => ({
-//   element: document.querySelector(`.${selector}`),
-//   disabledClass: `${selector}--disabled`
-// }));
-// const formElements = document.querySelectorAll('fieldset, select');
+const forms = ['ad-form', 'map__filters'].map((selector) => ({
+  element: document.querySelector(`.${selector}`),
+  disabledClass: `${selector}--disabled`
+}));
+const formElements = document.querySelectorAll('fieldset, select');
 
-// const toggleForms = (activate) => {
-//   const method = activate ? 'remove' : 'add';
-//   forms.forEach(({ element, disabledClass }) => {
-//     element.classList[method](disabledClass);
-//   });
+const toggleForms = (activate) => {
+  const method = activate ? 'remove' : 'add';
+  forms.forEach(({ element, disabledClass }) => {
+    element.classList[method](disabledClass);
+  });
 
-//   formElements.forEach((element) => element.disabled === !activate);
-// };
+  formElements.forEach((element) => element.disabled === !activate);
+};
 
-// const validateCapacity = () => ROOM_NUMBERS_GUESTS[roomNumberElement.value].includes(capacityElement.value);
+const titleElement = document.querySelector('#title');
+const priceElement = document.querySelector('#price');
+const roomNumberElement = document.querySelector('#room_number');
+const capacityElement = document.querySelector('#capacity');
+const typeHousingElement = document.querySelector('#type');
 
+const dataElements = {
+  forms: forms,
+  titleElement: titleElement,
+  priceElement: priceElement,
+  roomNumberElement: roomNumberElement,
+  capacityElement: capacityElement,
+  typeHousingElement: typeHousingElement
+};
 
-// const validateTypeHousing = (evt) => {
-//   priceElement.placeholder = evt.target.value;
-//   priceElement.value = evt.target.value;
+const validateCapacity = () => ROOM_NUMBERS_GUESTS[roomNumberElement.value].includes(capacityElement.value);
 
-//   if (priceElement.value >= PRICE_DICTIONARY[evt.target.value]) {
-//     return true;
-//   }
+const validateTypeHousing = (evt) => {
+  priceElement.placeholder = evt.target.value;
+  priceElement.value = evt.target.value;
 
-//   return false;
-// };
+  if (priceElement.value >= PRICE_DICTIONARY[evt.target.value]) {
+    return true;
+  }
 
-// roomNumberElement.addEventListener('change', validateCapacity);
-// typeHousingElement.addEventListener('change', validateTypeHousing);
+  return false;
+};
 
-// const validator = initValidator(forms);
+roomNumberElement.addEventListener('change', validateCapacity);
+typeHousingElement.addEventListener('change', validateTypeHousing);
 
-// export const activateForms = () => toggleForms(true);
-// export const deactivateForms = () => toggleForms(false);
-// export { validateCapacity, validateTypeHousing };
+initValidator(dataElements);
+
+export const activateForms = () => toggleForms(true);
+export const deactivateForms = () => toggleForms(false);
+export { validateCapacity, validateTypeHousing };
