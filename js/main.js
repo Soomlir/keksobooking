8249e96fae9afdac5ee6a00a54e2603be9f69ea1
Soomlir <Soomlir@users.noreply.github.com>
2023-04-08 18:18:33 +0000

    deploy: a9b55d4f970472e8d251535687b849599f299d39

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { getRandomOffers } from './create-offer-data.js';
 import { generateCard } from './generate-card.js';
-// import { initValidator } from './validator.js';
 
 const mapElement = document.querySelector('#map-canvas');
 const [cardData] = getRandomOffers(1);
