64a26c3e2c9063c82b49574aa0b80ecd546daad3
Soomlir <Soomlir@yandex.ru>
2023-04-08 22:00:17 +0300

    validate in progress

--- public/js/validator.js
+++ public/js/validator.js
@@ -1,38 +1,38 @@
-// import { validateCapacity, validateTypeHousing } from './form.js';
-// import { MIN_TITLE_LENGTH, MAX_TITLE_LENGTH, MAX_VALUE_PRICE, PRIORITY } from './constants.js';
+import { validateCapacity, validateTypeHousing } from './form.js';
+import { MIN_TITLE_LENGTH, MAX_TITLE_LENGTH, MAX_VALUE_PRICE, PRIORITY } from './constants.js';
 
-// const initValidator = ({dataElements}) => {
-//   const validator = new Pristine(form, {
-//     // TODO нужные опции
-//   });
+const initValidator = ({
+  forms,
+  titleElement,
+  priceElement,
+  capacityElement,
+  typeHousingElement
+}) => {
+  const validator = new Pristine(forms, {
+    // TODO нужные опции
+  });
 
-//   const titleElement = document.querySelector('#title');
-//   const priceElement = document.querySelector('#price');
-//   const roomNumberElement = document.querySelector('#room_number');
-//   const capacityElement = document.querySelector('#capacity');
-//   const typeHousingElement = document.querySelector('#type');
+  validator.addValidator(
+    titleElement,
+    ({ length }) => length >= MIN_TITLE_LENGTH && length <= MAX_TITLE_LENGTH,
+    `Не менее ${MIN_TITLE_LENGTH} символов и не более ${MAX_TITLE_LENGTH} символов`,
+    PRIORITY,
+    false
+  );
 
-//   validator.addValidator(
-//     titleElement,
-//     ({ length }) => length >= MIN_TITLE_LENGTH && length <= MAX_TITLE_LENGTH,
-//     `Не менее ${MIN_TITLE_LENGTH} символов и не более ${MAX_TITLE_LENGTH} символов`,
-//     PRIORITY,
-//     false
-//   );
+  validator.addValidator(
+    priceElement,
+    (value) => value <= MAX_VALUE_PRICE,
+    `Максимальное значение ${MAX_VALUE_PRICE}`,
+    PRIORITY,
+    false
+  );
 
-//   validator.addValidator(
-//     priceElement,
-//     (value) => value <= MAX_VALUE_PRICE,
-//     `Максимальное значение ${MAX_VALUE_PRICE}`,
-//     PRIORITY,
-//     false
-//   );
+  validator.addValidator(capacityElement, validateCapacity, '', PRIORITY, false);
 
-//   validator.addValidator(capacityElement, validateCapacity, '', PRIORITY, false);
+  validator.addValidator(typeHousingElement, validateTypeHousing, '', PRIORITY, false);
 
-//   validator.addValidator(priceElement, validateTypeHousing, '', PRIORITY, false);
+  return validator;
+};
 
-//   return validator;
-// };
-
-// export { initValidator };
+export { initValidator };
