75947d8473408079d37c9a0ba0500053f806611c
Soomlir <Soomlir@yandex.ru>
2023-06-23 22:36:37 +0300

    8rd task complete

--- public/js/validator.js
+++ public/js/validator.js
@@ -9,17 +9,24 @@ import {
 import { formElements } from './ad-form.js';
 
 const ERROR_TITLE = `Не менее ${MIN_TITLE_LENGTH} символов и не более ${MAX_TITLE_LENGTH} символов`;
-const ERROR_PRICE = `Максимальное значение ${MAX_VALUE_PRICE}`;
-const ERROR_CAPACITY = `Максимальное количество мест ${ROOM_NUMBERS_GUESTS['formElements.roomNumberElement.value']}`;
-const ERROR_CHECK_TIMEOUT = 'Время заезда и выезда должно быть одинаковое';
-const getErrorMinPrice = (value) => `Минимальная цена за ночь ${PRICE_DICTIONARY[value]}`;
+
+const showErrorPrice = (value) => {
+  if (value > MAX_VALUE_PRICE) {
+    return `Максимальное значение - ${MAX_VALUE_PRICE}`;
+  }
+
+  return `Минимальное значение - ${PRICE_DICTIONARY[formElements.typeHousingElement.value]}`;
+};
+
+const showErrorCapacity = () => {
+  const count = ROOM_NUMBERS_GUESTS[formElements.roomNumberElement.value];
+  return `Максимальное количество мест ${count[count.length - 1]}`;
+};
 
 const validateTitle = ({ length }) => length >= MIN_TITLE_LENGTH && length <= MAX_TITLE_LENGTH;
-const validatePrice = (value) => value <= MAX_VALUE_PRICE;
+const validatePrice = (value) => value >= PRICE_DICTIONARY[formElements.typeHousingElement.value] && value <= MAX_VALUE_PRICE;
 const validateRoomsNumber = () =>
   ROOM_NUMBERS_GUESTS[formElements.roomNumberElement.value].includes(formElements.capacityElement.value);
-const validateTypeHousing = () => formElements.typeHousingElement.value >= PRICE_DICTIONARY[formElements.priceElement.value];
-const validateTimeOut = () => formElements.timeInElement.value === formElements.timeOutElement.value;
 
 const initValidator = (formElement) => {
   const validator = new Pristine(formElement.formElement, {
@@ -28,11 +35,9 @@ const initValidator = (formElement) => {
     errorTextClass: 'ad-form__error-text'
   });
 
-  validator.addValidator(formElements.titleElement, validateTitle, ERROR_TITLE, PRIORITY, false);
-  validator.addValidator(formElements.priceElement, validatePrice, ERROR_PRICE, PRIORITY, false);
-  validator.addValidator(formElements.capacityElement, validateRoomsNumber, ERROR_CAPACITY, PRIORITY, false);
-  validator.addValidator(formElements.typeHousingElement, validateTypeHousing, getErrorMinPrice, PRIORITY, false);
-  validator.addValidator(formElements.timeOutElement, validateTimeOut, ERROR_CHECK_TIMEOUT, PRIORITY, false);
+  validator.addValidator(formElements.titleElement, validateTitle, ERROR_TITLE, PRIORITY, true);
+  validator.addValidator(formElements.priceElement, validatePrice, showErrorPrice, PRIORITY, true);
+  validator.addValidator(formElements.capacityElement, validateRoomsNumber, showErrorCapacity, PRIORITY, true);
 
   return validator;
 };
