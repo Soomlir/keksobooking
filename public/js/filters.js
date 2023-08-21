fc1c9e92e10f963f7ccddafa8d72fd9550d37639
Soomlir <Soomlir@yandex.ru>
2023-08-21 15:53:09 +0300

    12rd edits

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,7 +1,67 @@
-// import { getData } from './api.js';
+import { getData } from './api.js';
+import { createOfferData } from './create-offer-data.js';
+import { debounce } from './helpers.js';
+import { activateForms } from './toggle-page.js';
 
-// const filtersElement = document.querySelector('.map__filters');
+const RERENDER_DELAY = 500;
+const DEFAULT_VALUE = 'any';
+const filtersElement = document.querySelector('.map__filters');
+const filterControlGroups = Array.from(filtersElement.children);
 
-// const filterOffers = getData().then((data) => data.slice(0, 10));
+const dataFilterOffers = getData().then((data) => data.slice(0, 10));
+const housingPrice = {
+  low: {
+    from: 0,
+    to: 10000
+  },
+  middle: {
+    from: 10000,
+    to: 50000
+  },
+  high: {
+    from: 50000,
+    to: Infinity
+  }
+};
 
-// export { filterOffers };
+const filterRules = {
+  'housing-type': ({ type }, value) => value === type,
+  'housing-price': ({ price }, value) => price >= housingPrice[value].from && price < housingPrice[value].to,
+  'housing-rooms': ({ rooms }, value) => value === rooms.toString(),
+  'housing-guests': ({ guests }, value) => value === guests.toString(),
+  'housing-features': ({ features }) => {
+    if (!features) {
+      return false;
+    }
+    const checkedCheckboxes = Array.from(filtersElement.querySelectorAll('[type="checkbox"]:checked'));
+    return checkedCheckboxes.every(({ value }) => features.some((feature) => feature === value));
+  }
+};
+
+const filterOffers = ({ offer }) =>
+  filterControlGroups.every(({ value, id }) => value === DEFAULT_VALUE || filterRules[id](offer, value));
+
+const toggleFilters = () => {
+  activateForms();
+};
+
+const rerender = () => createOfferData(filterOffers);
+
+const clearFilters = () => {
+  filterControlGroups.forEach((group) => {
+    if (group.name) {
+      group.value = DEFAULT_VALUE;
+    } else {
+      group.querySelectorAll('input').forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+    }
+  });
+  rerender();
+};
+
+filtersElement.addEventListener('change', debounce(rerender, RERENDER_DELAY));
+
+export { toggleFilters, clearFilters, filterOffers };
+
+export { dataFilterOffers };
