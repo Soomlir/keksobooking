da75e94e2529deadb617b168ddf891819c93e5a3
Soomlir <Soomlir@users.noreply.github.com>
2023-08-21 11:21:55 +0000

    deploy: 6c03a6fedaf9053f0c733ff5a244b1b8bcad8a99

---
+++ js/filters.js
@@ -0,0 +1,7 @@
+// import { getData } from './api.js';
+
+// const filtersElement = document.querySelector('.map__filters');
+
+// const filterOffers = getData().then((data) => data.slice(0, 10));
+
+// export { filterOffers };
