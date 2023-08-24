d6bf8f7b855a030858380976e94e35791fb8c864
Soomlir <Soomlir@yandex.ru>
2023-08-24 22:43:35 +0300

    12rd complete

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,10 @@ import { getData } from './api.js';
 import { initMap } from './map.js';
 import { activateForms } from './toggle-page.js';
 import { initFilters } from './filters.js';
+import { previewAvatar, previewHousing } from './image-control.js';
+
+previewAvatar();
+previewHousing();
 
 getData().then((data) => {
   const resetMap = initMap(data, activateForms);
